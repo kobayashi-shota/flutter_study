792a6090cc3389b6fb618f77b36ba49e7a45993f
kobayashi-shota <kobayashi_shota@retail-ai.jp>
2023-06-02 14:58:16 +0900

    update: 100までカウントするとダイアログを表示する

---
+++ lib/pages/count_up_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class CountUpPage extends StatefulWidget {
+  const CountUpPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<CountUpPage> createState() => _CountUpPageState();
+}
+
+class _CountUpPageState extends State<CountUpPage> {
+  int _counter = 0;
+
+  int get limit => 100;
+
+  bool _isLimit() {
+    return _counter == limit;
+  }
+
+  Future<void> _resetCountAndPop() async {
+    setState(() {
+      _counter = 0;
+    });
+
+    Navigator.pop(context);
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      if (_isLimit()) {
+        _showLimitDialog();
+        return;
+      }
+
+      _counter++;
+    });
+  }
+
+  void _showLimitDialog() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) =>
+            SimpleDialog(title: const Text('お疲れ様でした🎉'), children: [
+              SimpleDialogOption(
+                onPressed: _resetCountAndPop,
+                child: const Text(
+                  'カウントをリセット',
+                  style: TextStyle(
+                      color: Colors.redAccent,
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              SimpleDialogOption(
+                onPressed: () => Navigator.pop(context),
+                child: const Text(
+                  'キャンセル',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.normal),
+                ),
+              ),
+            ]));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text(
+          '現在のカウント: $_counter',
+          style: const TextStyle(fontSize: 32.0, fontWeight: FontWeight.w400),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
