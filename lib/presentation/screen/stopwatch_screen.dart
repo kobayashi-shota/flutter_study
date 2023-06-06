f9b88898b5d40df9c94247379b22adbd7695ae48
kobayashi-shota <kobayashi_shota@retail-ai.jp>
2023-06-06 11:02:08 +0900

    fix: use_key_in_widget_constructors

--- lib/presentation/screen/stopwatch_screen.dart
+++ lib/presentation/screen/stopwatch_screen.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 class StopwatchScreen extends StatefulWidget {
+  const StopwatchScreen({super.key});
+
   @override
   State<StopwatchScreen> createState() => _StopwatchScreenState();
 }
