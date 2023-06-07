a470418745520f26d31b322690585213509ca64c
kobayashi-shota <kobayashi_shota@retail-ai.jp>
2023-06-07 17:04:47 +0900

    fix: unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_study/presentation/screen/count_up_screen.dart';
 import 'package:flutter_study/presentation/screen/search_books_top_screen.dart';
 
 void main() {
