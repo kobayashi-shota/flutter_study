10c954cb8473c08edba66d0ee59cee4bc968ef58
Shota Kobayashi <82574685+kobayashi-shota@users.noreply.github.com>
2023-06-07 10:31:01 +0900

    fix: 課題1.ボタンを押すとカウントアップされるシンプルなカウンターアプリを作成する (#7)
    * fix: 課題1.ボタンを押すとカウントアップされるシンプルなカウンターアプリを作成する #1
    
    * update: 100までカウントするとダイアログを表示する
    
    * fix: setState内ではStateのプロパティの更新だけ行う
    
    * update dependencies pedantic_mono: any
    
    * update analysis_options(pedantic_mono)
    
    * fix: lint警告(count_up_page)
    
    * fix: lint警告(Unsorted dependencies)
    
    * fix: lint警告(Sort directive sections alphabetically.)
    
    * fix: limitをfinal宣言に修正
    
    * fix: isLimitをgetterに修正
    
    * update: ファイル名とディレクトリ構成のリファクタリング

---
+++ lib/presentation/screen/count_up_screen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class CountUpScreen extends StatefulWidget {
+  const CountUpScreen({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<CountUpScreen> createState() => _CountUpScreenState();
+}
+
+class _CountUpScreenState extends State<CountUpScreen> {
+  int _counter = 0;
+
+  final int limit = 100;
+
+  bool get _isLimit {
+    return _counter >= limit;
+  }
+
+  Future<void> _resetCountAndPop() async {
+    setState(() {
+      _counter = 0;
+    });
+
+    Navigator.pop(context);
+  }
+
+  void _incrementCounter() {
+    if (_isLimit) {
+      _showLimitDialog();
+      return;
+    }
+
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _showLimitDialog() {
+    showDialog<void>(
+        context: context,
+        builder: (BuildContext context) =>
+            SimpleDialog(title: const Text('お疲れ様でした🎉'), children: [
+              SimpleDialogOption(
+                onPressed: _resetCountAndPop,
+                child: const Text(
+                  'カウントをリセット',
+              style: TextStyle(
+                color: Colors.redAccent,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+              ),
+          SimpleDialogOption(
+            onPressed: () => Navigator.pop(context),
+            child: const Text(
+              'キャンセル',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 24,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text(
+          '現在のカウント: $_counter',
+          style: const TextStyle(fontSize: 32, fontWeight: FontWeight.w400),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
