4c8ef934ed376bf5cb15b086c2da2ddce836dbfa
kobayashi-shota <kobayashi_shota@retail-ai.jp>
2023-06-16 11:16:31 +0900

    fix: 課題5. GitHub APIを利用して、指定したリポジトリの情報を取得し、リスト形式で表示するアプリを作成する #5

---
+++ lib/presentation/screen/search_github_repo_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_study/presentation/screen/search_github_result_screen.dart';
+
+class SearchGithubHomeScreen extends StatefulWidget {
+  const SearchGithubHomeScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _SearchGithubHomeScreenState();
+}
+
+class _SearchGithubHomeScreenState extends State<SearchGithubHomeScreen> {
+  final TextEditingController _textEditingController = TextEditingController();
+  final _searchedList = <String>[];
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('')),
+      body: SafeArea(
+        child: TextField(
+          controller: _textEditingController,
+          decoration: const InputDecoration(label: Text('GitHubでリポジトリを検索')),
+          onEditingComplete: () {
+            _searchedList.add(_textEditingController.text);
+            Navigator.push(
+              context,
+              MaterialPageRoute<void>(
+                builder: (context) => SearchGithubResultScreen(
+                  query: _textEditingController.text.trim(),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/screen/search_github_result_screen.dart
@@ -0,0 +1,91 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_study/domain/entity/github_repo.dart';
+import 'package:flutter_study/network/clients/github_api_client.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+
+class SearchGithubResultScreen extends StatefulWidget {
+  const SearchGithubResultScreen({super.key, required this.query});
+
+  final String query;
+
+  @override
+  State<StatefulWidget> createState() => _SearchGithubResultScreenState();
+}
+
+class _SearchGithubResultScreenState extends State<SearchGithubResultScreen> {
+  late final PagingController<int, GithubRepo> _pagingController;
+  int currentPage = 1;
+  bool isLoading = false;
+  final List<GithubRepo> itemList = [];
+  final dio = Dio();
+  late final GithubApiClient client;
+
+  Future<void> fetchItems() async {
+    final newData = await client.getGitHubRepoList(widget.query, currentPage);
+    setState(() {
+      itemList.addAll(newData.items);
+      currentPage++;
+    });
+
+    try {
+      final hasNext = newData.items.isNotEmpty;
+      if (hasNext) {
+        _pagingController.appendPage(newData.items, currentPage);
+      } else {
+        _pagingController.appendLastPage(newData.items);
+      }
+    } on Exception catch (e) {
+      _pagingController.error = e;
+    }
+  }
+
+  @override
+  void initState() {
+    client = GithubApiClient(dio);
+    _pagingController = PagingController(firstPageKey: currentPage);
+    _pagingController.addPageRequestListener((pageKey) {
+      fetchItems();
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pagingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '${widget.query}を含むリポジトリ',
+          style: const TextStyle(fontSize: 16),
+        ),
+      ),
+      body: SafeArea(
+        child: PagedListView<int, GithubRepo>(
+          pagingController: _pagingController,
+          builderDelegate: PagedChildBuilderDelegate<GithubRepo>(
+            itemBuilder: (context, repo, index) {
+              return ListTile(
+                title: Text(repo.name),
+                subtitle: Text(repo.description ?? ''),
+                leading: CachedNetworkImage(
+                  imageUrl: repo.owner.avatarUrl!,
+                  placeholder: (context, url) =>
+                      const CircularProgressIndicator(),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                ),
+                trailing: Text('⭐️${repo.stargazersCount}'),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
